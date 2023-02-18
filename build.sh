c85ebf72663a767b94ee447a5c57bed52eda45d1
sh4 <sh4@users.noreply.github.com>
2023-02-18 22:54:55 +0900

    Add GitHub Actions workflow files.

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-code_web_package="/app/out/vscode-web-$(git -C /app/vscode name-rev --name-only HEAD | sed -e 's/tags\///g' | sed -e 's/\//-/g').zip"
+if [ -z "$BUILD_ROOT" ]; then
+  BUILD_ROOT=/app
+fi
 
-yarn --cwd /app/vscode &&
-yarn --cwd /app/vscode gulp vscode-web-min &&
-mkdir -p /app/out && \
+code_web_package="$BUILD_ROOT/out/vscode-web-$(git -C $BUILD_ROOT/vscode name-rev --name-only HEAD | sed -e 's/tags\///g' | sed -e 's/\//-/g').zip"
+
+yarn --cwd $BUILD_ROOT/vscode &&
+yarn --cwd $BUILD_ROOT/vscode gulp vscode-web-min &&
+mkdir -p $BUILD_ROOT/out && \
 rm -f $code_web_package && \
-( cd /app/vscode-web && zip -1 -r $code_web_package . )
+( cd $BUILD_ROOT/vscode-web && zip -1 -r $code_web_package . )
